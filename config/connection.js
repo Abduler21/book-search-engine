21bc92a865bd43cc031f1c4a6bc83c543bb43ddd
Abdulmelik Ersoy <kellemen23@gmail.com>
2022-03-27 03:36:01 +0300

    added mongodb server

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,13 @@
-const mongoose = require("mongoose");
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://abdulersy:7A78RLMZTbeohzzp@cluster0.fu1ww.mongodb.net/bookenginedb?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
-
-module.exports = mongoose.connection;
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
