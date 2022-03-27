1a189f85da3739ba4faded6ce20a220140635642
Abdulmelik Ersoy <kellemen23@gmail.com>
2022-03-27 23:47:37 +0300

    edited database

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/searchengine",
+  process.env.MONGODB_URI || "mongodb://localhost/bookenginedb",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
