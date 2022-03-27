860b479babeb040a1fe568b9dc86bcb3be4b1c19
Abdulmelik Ersoy <kellemen23@gmail.com>
2022-03-27 04:46:36 +0300

    added files

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
-const path = require('path');
+const path = require("path");
 const { typeDefs, resolvers } = require("./schemas");
-const db = require('./config/connection');
-const { authMiddleware } = require('./utils/auth');
+const db = require("./config/connection");
+const { authMiddleware } = require("./utils/auth");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -19,15 +19,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-  console.log(`GraphQL server ready at http://localhost:${PORT}${server.graphqlPath}`);
+  console.log(
+    `GraphQL server ready at http://localhost:${PORT}${server.graphqlPath}`
+  );
 });
